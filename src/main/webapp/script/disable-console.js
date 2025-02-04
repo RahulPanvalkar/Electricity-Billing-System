dab79991125c314db00bbde803687f34c991924d
Rahul Panvalkar <rahulpanvalkar59@gmail.com>
2025-02-04 23:39:15 +0530

    make changes to disable console logs

--- src/main/webapp/script/disable-console.js
+++ src/main/webapp/script/disable-console.js
@@ -1,8 +1,8 @@
-// To disable console in the production env
+// To disable console logs in the production env
 (function() {
     var noop = function() {};
-    //var methods = ['log', 'warn', 'error', 'info', 'debug'];
-    var methods = ['debug'];
+    var methods = ['log', 'warn', 'error', 'info', 'debug'];
+    //var methods = ['debug'];
     for (var i = 0; i < methods.length; i++) {
         console[methods[i]] = noop;
     }
